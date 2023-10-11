aa3e7898352aaa11fe8aef409306ffb4dfe3348b
sylee-skhu <90588040+sylee-skhu@users.noreply.github.com>
2023-10-11 16:53:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,54 +23,6 @@ def main():
     dino_bottom = MAX_HEIGHT - dino_height
     dino_x = 50
     dino_y = dino_bottom
-    jump_top = 200
-    leg_swap = True
-    is_bottom = True
-    is_go_up = False
- 
-    # tree
-    imgTree = pygame.image.load('img/tree.png')
-    tree_height = imgTree.get_size()[1]
-    tree_x = MAX_WIDTH
-    tree_y = MAX_HEIGHT - tree_height
- 
-    while True:
-        screen.fill((255, 255, 255))
- 
-        # event check
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if is_bottom:
-                    is_go_up = True
-                    is_bottom = False
- 
-        # dino move
-        if is_go_up:
-            dino_y -= 10.0
-        elif not is_go_up and not is_bottom:
-            dino_y += 10.0
- 
-        # dino top and bottom check
-        if is_go_up and dino_y <= jump_top:
-            is_go_up = False
- 
-        if not is_bottom and dino_y >= dino_bottom:
-            is_bottom = True
-            dino_y = dino_bottom
- 
-        # tree move
-        tree_x -= 12.0
-        if tree_x <= 0:
-            tree_x = MAX_WIDTH
- 
-        # draw tree
-        screen.blit(imgTree, (tree_x, tree_y))
- 
-        # draw dino
-        if leg_swap:
             screen.blit(imgDino1, (dino_x, dino_y))
             leg_swap = False
         else:
